de62b90a1537a396efef41481062d25c52872cf9
vinayako9 <111178152+vinayako9@users.noreply.github.com>
2022-10-04 14:01:52 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,25 @@
+class Paper {
+constructor(x,y,radius);{
+isStatic: false,
+
+ var options= {
+     isStatic: false,
+	 'restitution':0.3,
+	 'friction':0.5,
+	 'density':1.2		
+  }
+ this.body = Bodies.circle(x,y,radius,options);
+ this.radius = radius
+
+ World.add(world, this.body);
+
+}
+ display() {		
+ rectMode(CENTER)
+	 
+ Fill("yellow");
+
+ circle(this.body.position.x, this.body.position.y, this.radius );
+			
+}
+};
